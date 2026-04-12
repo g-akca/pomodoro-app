2daf5e015a242787e1ca812fc5d8edce620a0bd0
g-akca <gok.akca66@gmail.com>
2026-04-12 22:22:43 +0300

    Animate tabs with duration

--- src/components/TabContainer.jsx
+++ src/components/TabContainer.jsx
@@ -7,9 +7,9 @@ function TabContainer() {
     <div className="self-stretch bg-blue-900 rounded-[31.5px] p-[7.5px] h-15.75">
       <div
         className={`
-          relative h-full grid grid-cols-3 font-bold text-blue-100
-          before:content-[''] before:absolute before:top-0 before:left-0
-          before:h-12 before:w-1/3 before:rounded-[26.5px] before:bg-red-400 before:transition-all
+          relative h-full grid grid-cols-3 font-bold text-blue-100/40
+          before:content-[''] before:absolute before:top-0 before:left-0 before:h-12 before:w-1/3 
+          before:rounded-[26.5px] before:bg-red-400 before:transition-all before:duration-400
           ${
             mode === "pomodoro" ? "before:translate-x-0"
             : mode === "short"? "before:translate-x-full"
@@ -17,19 +17,19 @@ function TabContainer() {
           }
         `}
       >
-        <label className="relative z-10 flex justify-center items-center has-checked:text-blue-850">
+        <label className="relative z-10 flex justify-center items-center transition-all delay-100 duration-100 has-checked:text-blue-850">
           <input type="radio" name="mode" value="pomodoro" onChange={(e) => setMode(e.target.value)} className="hidden peer" defaultChecked />
-          <p className="lowercase opacity-40 peer-checked:opacity-100">Pomodoro</p>
+          <p className="lowercase">Pomodoro</p>
         </label>
 
-        <label className="relative z-10 flex justify-center items-center has-checked:text-blue-850">
+        <label className="relative z-10 flex justify-center items-center transition-all delay-100 duration-100 has-checked:text-blue-850">
           <input type="radio" name="mode" value="short" onChange={(e) => setMode(e.target.value)} className="hidden peer" />
-          <p className="lowercase opacity-40 peer-checked:opacity-100">Short break</p>
+          <p className="lowercase">Short break</p>
         </label>
 
-        <label className="relative z-10 flex justify-center items-center has-checked:text-blue-850">
+        <label className="relative z-10 flex justify-center items-center transition-all delay-100 duration-100 has-checked:text-blue-850">
           <input type="radio" name="mode" value="long" onChange={(e) => setMode(e.target.value)} className="hidden peer" />
-          <p className="lowercase opacity-40 peer-checked:opacity-100">Long break</p>
+          <p className="lowercase">Long break</p>
         </label>
       </div>
     </div>
