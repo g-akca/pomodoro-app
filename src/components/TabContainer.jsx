022828d885b8f185b2c1462ac146e831446c40bb
g-akca <gok.akca66@gmail.com>
2026-04-12 21:51:42 +0300

    Add mode state and tab styles

--- src/components/TabContainer.jsx
+++ src/components/TabContainer.jsx
@@ -1,19 +1,23 @@
+import { useState } from "react";
+
 function TabContainer() {
+  const [mode, setMode] = useState("pomodoro");
+
   return (
-    <div className="self-stretch bg-blue-900 rounded-[31.5px] p-[7.5px] h-15.75 grid grid-cols-3 font-bold">
-      <label className="flex justify-center items-center bg-red-400 rounded-[26.5px] text-blue-850">
-        <input type="radio" className="hidden" defaultChecked />
-        <p className="lowercase">Pomodoro</p>
+    <div className="self-stretch bg-blue-900 rounded-[31.5px] p-[7.5px] h-15.75 grid grid-cols-3 font-bold text-blue-100">
+      <label className="flex justify-center items-center rounded-[26.5px] has-checked:text-blue-850 has-checked:bg-red-400">
+        <input type="radio" name="mode" value="pomodoro" onChange={(e) => setMode(e.target.value)} className="hidden peer" defaultChecked />
+        <p className="lowercase opacity-40 peer-checked:opacity-100">Pomodoro</p>
       </label>
 
-      <label className="flex justify-center items-center rounded-[26.5px] text-blue-100">
-        <input type="radio" className="hidden" />
-        <p className="lowercase opacity-40">Short break</p>
+      <label className="flex justify-center items-center rounded-[26.5px] has-checked:text-blue-850 has-checked:bg-red-400">
+        <input type="radio" name="mode" value="short" onChange={(e) => setMode(e.target.value)} className="hidden peer" />
+        <p className="lowercase opacity-40 peer-checked:opacity-100">Short break</p>
       </label>
 
-      <label className="flex justify-center items-center rounded-[26.5px] text-blue-100">
-        <input type="radio" className="hidden" />
-        <p className="lowercase opacity-40">Long break</p>
+      <label className="flex justify-center items-center rounded-[26.5px] has-checked:text-blue-850 has-checked:bg-red-400">
+        <input type="radio" name="mode" value="long" onChange={(e) => setMode(e.target.value)} className="hidden peer" />
+        <p className="lowercase opacity-40 peer-checked:opacity-100">Long break</p>
       </label>
     </div>
   )
