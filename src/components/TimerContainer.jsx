efb66a0f10481663f267852b14698f87b7d63ee5
g-akca <gok.akca66@gmail.com>
2026-04-13 15:34:37 +0300

    Add inner circle to TimerContainer, change bg direction

--- src/components/TimerContainer.jsx
+++ src/components/TimerContainer.jsx
@@ -1,6 +1,9 @@
 function TimerContainer() {
   return (
-    <div className="h-75 aspect-square rounded-full bg-linear-315 from-[#2E325A] to-[#0E112A] shadow-[-50px_-50px_100px_#272C5A,50px_50px_100px_#121530]">
+    <div className="h-75 aspect-square p-4 rounded-full bg-linear-275 from-[#2E325A] to-[#0E112A] shadow-[-50px_-50px_100px_#272C5A,50px_50px_100px_#121530]">
+      <div className="w-full h-full bg-blue-900 rounded-full">
+
+      </div>
     </div>
   )
 }
