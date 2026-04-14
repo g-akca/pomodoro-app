811a8772938f872894fed8b70e1cfeb61a4d47c7
g-akca <gok.akca66@gmail.com>
2026-04-14 23:53:31 +0300

    Create SettingsContext

---
+++ src/context/SettingsContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useContext, useState } from "react";
+
+const SettingsContext = createContext();
+
+export function SettingsProvider({ children }) {
+  const [settings, setSettings] = useState({
+    duration: {
+      pomodoro: 25,
+      short: 5,
+      long: 15
+    },
+    font: "sans",
+    color: "red"
+  });
+
+  return (
+    <SettingsContext.Provider value={{ settings, setSettings }}>
+      {children}
+    </SettingsContext.Provider>
+  )
+}
+
+export function useSettings() {
+  return useContext(SettingsContext);
+}
