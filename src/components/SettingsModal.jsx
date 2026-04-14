4353fa2e03b2f256c119f2f053391d4c1856a709
g-akca <gok.akca66@gmail.com>
2026-04-14 22:55:31 +0300

    Add FontAwesomeIcon for tick icon, position the apply button

--- src/components/SettingsModal.jsx
+++ src/components/SettingsModal.jsx
@@ -1,10 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+
 import closeIcon from "/assets/icon-close.svg";
 
 function SettingsModal({ closeModal }) {
   return (
     <div className="fixed inset-0 bg-blue-950 z-40 flex items-center justify-center p-6">
       <div className="bg-white w-full max-w-140 rounded-[15px] flex flex-col gap-4">
-        <div className="flex justify-between py-4 px-6 border-b border-grey-200">
+        <div className="flex justify-between pt-4.5 pb-4 px-6 border-b border-grey-200">
           <h1 className="text-[28px] leading-base font-bold text-blue-900">Settings</h1>
 
           <button type="button" onClick={closeModal}>
@@ -14,23 +17,23 @@ function SettingsModal({ closeModal }) {
 
         <div className="px-6">
           <div className="pb-6 border-b border-grey-200 flex flex-col gap-4">
-            <h2 className="text-blue-900 font-bold text-[11px] leading-[100%] tracking-[4.23px] uppercase text-center">Time (Minutes)</h2>
+            <h2 className="text-blue-900 font-bold text-[11px] leading-3.5 tracking-[4.23px] uppercase text-center">Time (Minutes)</h2>
           
             <div className="flex flex-col gap-2">
               <div className="flex justify-between items-center">
-                <label for="pomodoro" className="opacity-40 lowercase font-bold">Pomodoro</label>
+                <label htmlFor="pomodoro" className="opacity-40 lowercase font-bold">Pomodoro</label>
   
                 <input type="number" className="w-35 h-10 px-4 bg-blue-50 rounded-[10px] text-[13px] leading-base tracking-[3px] font-bold" />
               </div>
 
               <div className="flex justify-between items-center">
-                <label for="pomodoro" className="opacity-40 lowercase font-bold">Short break</label>
+                <label htmlFor="pomodoro" className="opacity-40 lowercase font-bold">Short break</label>
   
                 <input type="number" className="w-35 h-10 px-4 bg-blue-50 rounded-[10px] text-[13px] leading-base tracking-[3px] font-bold" />
               </div>
 
               <div className="flex justify-between items-center">
-                <label for="pomodoro" className="opacity-40 lowercase font-bold">Long break</label>
+                <label htmlFor="pomodoro" className="opacity-40 lowercase font-bold">Long break</label>
   
                 <input type="number" className="w-35 h-10 px-4 bg-blue-50 rounded-[10px] text-[13px] leading-base tracking-[3px] font-bold" />
               </div>
@@ -38,7 +41,7 @@ function SettingsModal({ closeModal }) {
           </div>
 
           <div className="py-6 border-b border-grey-200 flex flex-col gap-4">
-            <h2 className="text-blue-900 font-bold text-[11px] leading-[100%] tracking-[4.23px] uppercase text-center">Font</h2>
+            <h2 className="text-blue-900 font-bold text-[11px] leading-3.5 tracking-[4.23px] uppercase text-center">Font</h2>
           
             <div className="flex gap-4 justify-center text-[16px] leading-[100%] text-blue-850/73">
               <label className="w-10 aspect-square rounded-full flex justify-center items-center bg-blue-50 font-sans font-bold has-checked:bg-blue-900 has-checked:text-white">
@@ -59,28 +62,28 @@ function SettingsModal({ closeModal }) {
           </div>
 
           <div className="pt-6 flex flex-col gap-4">
-            <h2 className="text-blue-900 font-bold text-[11px] leading-[100%] tracking-[4.23px] uppercase text-center">Color</h2>
+            <h2 className="text-blue-900 font-bold text-[11px] leading-3.5 tracking-[4.23px] uppercase text-center">Color</h2>
           
             <div className="flex gap-4 justify-center">
               <label className="w-10 aspect-square rounded-full flex justify-center items-center bg-red-400">
                 <input type="radio" name="color" value="red" className="hidden peer" defaultChecked />
-                <p className="hidden peer-checked:block text-[20px]">+</p>
+                <FontAwesomeIcon icon={faCheck} className="opacity-0 peer-checked:opacity-100 text-[16px]" />
               </label>
 
               <label className="w-10 aspect-square rounded-full flex justify-center items-center bg-cyan-300">
                 <input type="radio" name="color" value="cyan" className="hidden peer" />
-                <p className="hidden peer-checked:block text-[20px]">+</p>
+                <FontAwesomeIcon icon={faCheck} className="opacity-0 peer-checked:opacity-100 text-[16px]" />
               </label>
 
               <label className="w-10 aspect-square rounded-full flex justify-center items-center bg-purple-400">
                 <input type="radio" name="color" value="purple" className="hidden peer" />
-                <p className="hidden peer-checked:block text-[20px]">+</p>
+                <FontAwesomeIcon icon={faCheck} className="opacity-0 peer-checked:opacity-100 text-[16px]" />
               </label>
             </div>
           </div>
         </div>
 
-        <button type="button" className="bg-red-400 text-white text-[16px] leading-base font-bold w-35 h-13.25 self-center rounded-[26.5px]">Apply</button>
+        <button type="button" className="bg-red-400 text-white text-[16px] leading-base font-bold w-35 h-13.25 self-center rounded-[26.5px] translate-y-6.25 -mt-2">Apply</button>
       </div>
     </div>
   )
