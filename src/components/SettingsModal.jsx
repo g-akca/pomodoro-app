a9c20f31d33c31d8fccbc3d3102a8392f2473f26
g-akca <gok.akca66@gmail.com>
2026-04-14 19:06:32 +0300

    Add part of SettingsModal styles and structure

--- src/components/SettingsModal.jsx
+++ src/components/SettingsModal.jsx
@@ -1,7 +1,16 @@
 function SettingsModal() {
   return (
-    <>
-    </>
+    <div className="fixed inset-0 bg-blue-950 z-40 flex items-center justify-center p-6">
+      <div className="bg-white w-full max-w-140 rounded-[15px] flex flex-col gap-4">
+        <div>
+          <h1 className="py-4 px-6 border-b border-grey-200">Settings</h1>
+        </div>
+
+        <div>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
